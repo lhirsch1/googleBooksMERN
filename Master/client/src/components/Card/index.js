b187a77c8cf761b79ad0176b4695400d83878fab
Lukas Hirsch <hirschlukasa@gmail.com>
2020-05-07 11:39:58 -0500

    commented card component

--- Master/client/src/components/Card/index.js
+++ Master/client/src/components/Card/index.js
@@ -1,18 +1,22 @@
+//require dependency
 import React from "react";
 
+//functional component creates bootstrap card 
 function Card({ icon, title, children }) {
   return (
     <div className="card mt-4">
       <div className="card-header">
         <h3>
           <strong>
+            {/* displays favicon/title passed in through props */}
             <i className={`fa fa-${icon}`} aria-hidden="true" /> {title}
           </strong>
         </h3>
       </div>
+      {/* children is a prop that is used to store the element body */}
       <div className="card-body">{children}</div>
     </div>
   );
 }
-
+//export card component
 export default Card;
